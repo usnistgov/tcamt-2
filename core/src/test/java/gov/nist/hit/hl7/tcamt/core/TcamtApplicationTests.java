5a706c7b99282bd8fad2a71f93fed1cf4b2f99ab
Sukchan Lee <acetcom@gmail.com>
2020-04-13 22:56:01 -0400

    merge done

--- core/src/test/java/gov/nist/hit/hl7/tcamt/core/TcamtApplicationTests.java
+++ core/src/test/java/gov/nist/hit/hl7/tcamt/core/TcamtApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
