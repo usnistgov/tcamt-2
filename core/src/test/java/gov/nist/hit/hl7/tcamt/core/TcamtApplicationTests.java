bbe4ccf83a702f911563171f52dcff8ba0fc499a
Sukchan Lee <acetcom@gmail.com>
2020-04-13 22:58:01 -0400

    remove reduant

--- core/src/test/java/gov/nist/hit/hl7/tcamt/core/TcamtApplicationTests.java
+++ core/src/test/java/gov/nist/hit/hl7/tcamt/core/TcamtApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootTest
 class TcamtApplicationTests {
